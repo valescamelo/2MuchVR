1e306495eea3479ce3a6f307e6a5e208d69be694
Valesca M Oliveira <leskmelo@gmail.com>
2026-05-05 14:42:01 -0300

    Update musica.py

--- musica.py
+++ musica.py
@@ -19,35 +19,41 @@ pygame.mixer.music.play()
 
 letra = [
     (0, 'Say, "I love you" under my breath'),
-    (4, 'More times than you can digest'),
-    (8, 'Music every time I hear your name, oh'),
-    (13, "Your head's layin' right on my chest"),
-    (17, "Sun's up, but I still ain't got no rest"),
-    (21, "Don't wanna close my eyes, I'm scared I'll miss too much"),
-    (27, "Don't wanna fall asleep, I'd rather fall in love"),
-    (32, "When I can't feel you, I feel out of touch"),
-    (36, "Two seconds without you's like two months")
+    (3.5, 'More times than you can digest'),
+    (7.5, 'Music every time I hear your name, oh'),
+    (12.5, "Your head's layin' right on my chest"),
+    (16.5, "Sun's up, but I still ain't got no rest"),
+    (20.5, "Don't wanna close my eyes, I'm scared I'll miss too much"),
+    (26.5, "Don't wanna fall asleep, I'd rather fall in love"),
+    (31.5, "When I can't feel you, I feel out of touch"),
+    (35.5, "Two seconds without you's like two months")
 ]
 
 inicio = time.time()
 
-for tempo, linha in letra:
+for i in range(len(letra)):
+    tempo, linha = letra[i]
+
+    if i < len(letra) - 1:
+        proximo_tempo = letra[i + 1][0]
+    else:
+        proximo_tempo = tempo + 4
+
     while time.time() - inicio < tempo:
-        time.sleep(0.1)
+        time.sleep(0.005)
 
     os.system('cls')
 
-    linha_centrada = centralizar(linha)
-    
-#Sincronizando letra..
+    texto = linha
 
-    for letra_char in linha:
+    duracao = proximo_tempo - tempo
+    delay_por_letra = (duracao / len(texto)) * 1.0
+
+    for letra_char in texto:
         print(Fore.CYAN + letra_char, end="", flush=True)
-        time.sleep(0.08)
-    print(Style.RESET_ALL)
-    time.sleep(1)
+        time.sleep(delay_por_letra)
 
-#Aguardando a música terminar 
+    print(Style.RESET_ALL)
 
 while pygame.mixer.music.get_busy():
     time.sleep(1)
